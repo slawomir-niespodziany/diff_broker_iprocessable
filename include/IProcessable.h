a6933dcb19fa46404c2298fe2f5fb58c39ff527b
sniespod <sniespod@gmail.com>
2025-05-01 20:45:52 +0200

    Docs added.

--- include/IProcessable.h
+++ include/IProcessable.h
@@ -1,7 +1,17 @@
 #pragma once
 
+/**
+ * @brief Interface for processable components.
+ */
 class IProcessable {
 public:
+    /**
+     * @brief Indicate whether processing is finished.
+     */
     virtual bool finished() = 0;
+
+    /**
+     * @brief Perform processing step.
+     */
     virtual void process() = 0;
 };
